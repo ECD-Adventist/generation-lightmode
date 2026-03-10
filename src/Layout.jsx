16c69d6f4a318bcd0414c90b1d39c5d626deffbe
Innocent Wayoga <nottainnovation@gmail.com>
2026-03-10 20:28:31 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -403,6 +403,7 @@ export default function Layout({ children, currentPageName }) {
           </div>
         </div>
       </footer>
+      )}
 
       <style>{`
         @media (max-width: 768px) {
